833129de7e882ff4e7754865484e745b1c272197
mia elabed <maya.elabed22.05@gmail.com>
2023-11-23 11:31:25 +0100

    full project

---
+++ src/main/java/dev/simplesolution/rest/repositories/ContactRepository.java
@@ -0,0 +1,16 @@
+package dev.simplesolution.rest.repositories;
+
+import java.util.List;
+import dev.simplesolution.rest.entites.Contact;
+
+public interface ContactRepository {
+    List<Contact> getAll();
+
+    Contact getById(Long id);
+
+    Contact save(Contact newContact);
+
+    Contact update(Contact newContact);
+
+    void delete(Long id);
+}
